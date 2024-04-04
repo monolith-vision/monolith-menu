b8a7b0923433717a42f853ee0c51ed96cd21a3d5
ardelan <101870550+ardelan869@users.noreply.github.com>
2024-04-04 05:49:40 +0200

    feat: added main menu utilities

---
+++ client/main.lua
@@ -0,0 +1,40 @@
+local SendNUIMessage <const> = SendNUIMessage;
+
+exports('SendNUIMessage', function(message)
+  local resource = GetInvokingResource();
+
+  if resource == nil or resource == 'monolith-menu' then
+    return;
+  end
+
+  SendNUIMessage(message);
+end);
+
+exports('getLib', function()
+  return Console;
+end);
+
+local nuiCallbacks = {
+  'onChange',
+  'onCheck',
+  'onClick',
+  'onComponentSelect',
+  'Back',
+  'Exit'
+};
+
+for _, name in next, nuiCallbacks do
+  RegisterNUICallback(name, function(req, resp)
+    if not req.menu or not req.menu.__resource then
+      return resp('OK');
+    end
+
+    local import <const> = exports[req.menu.__resource];
+
+    if not import then
+      return resp('OK');
+    end
+
+    import:OnNUICallback(name, req, resp);
+  end);
+end
