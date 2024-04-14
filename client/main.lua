7f8476239552178da00a0ba2b043f2d8771ce27a
ardelan <101870550+ardelan869@users.noreply.github.com>
2024-04-14 12:18:32 +0200

    fix: Fixed Menu not closing, when the resource is stopped

--- client/main.lua
+++ client/main.lua
@@ -1,9 +1,13 @@
+local menuResources = {};
+
 exports('SendNUIMessage', function(message)
   local resource = GetInvokingResource();
 
   if resource == nil or resource == 'monolith-menu' then
     return;
   end
+
+  menuResources[resource] = true;
 
   SendNUIMessage(message);
 end);
@@ -20,6 +24,8 @@ local nuiCallbacks = {
 for _, name in next, nuiCallbacks do
   RegisterNUICallback(name, function(req, resp)
     if not req.menu or not req.menu.__resource then
+      SendNUIMessage({ action = 'SetMenu' });
+
       return resp('OK');
     end
 
@@ -32,3 +38,9 @@ for _, name in next, nuiCallbacks do
     import:OnNUICallback(name, req, resp);
   end);
 end
+
+AddEventHandler('onResourceStop', function(resource)
+  if menuResources[resource] then
+    SendNUIMessage({ action = 'SetMenu' });
+  end
+end);
