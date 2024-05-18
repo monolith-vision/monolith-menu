eb281c93f9e004a40c66382000a45074d63b9d1c
Ardelan <ardelanyamanel@outlook.de>
2024-05-18 23:27:17 +0200

    fix: moved Input Handling from `menu.lua`

---
+++ client/keys.lua
@@ -0,0 +1,39 @@
+Citizen.CreateThreadNow(function()
+  local keys = {
+    ArrowUp = { key = 188, mapper = 'UP' },
+    ArrowDown = { key = 187, mapper = 'DOWN' },
+    ArrowLeft = { key = 189, mapper = 'LEFT' },
+    ArrowRight = { key = 190, mapper = 'RIGHT' },
+    Enter = { key = 191, mapper = 'RETURN' },
+    Backspace = { key = 194, mapper = 'BACK' },
+    Escape = { key = 200, mapper = 'ESCAPE' }
+  };
+
+  ---@param name string
+  local function sendKey(name)
+    SendNUIMessage({ action = name });
+  end
+
+  for name, data in next, keys do
+    RegisterCommand('menu:' .. name, function()
+      if not Menu.current then
+        return;
+      end
+
+      DisableControlAction(0, data.key, true);
+      SetPauseMenuActive(false);
+
+      repeat
+        sendKey(name);
+
+        if name == 'Escape' then
+          return;
+        end
+
+        Citizen.Wait(120);
+      until IsDisabledControlReleased(0, data.key)
+    end, false);
+
+    RegisterKeyMapping('menu:' .. name, name, 'keyboard', data.mapper);
+  end
+end);
