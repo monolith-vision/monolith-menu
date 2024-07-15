f5c3dcdc63d9d87fa4e79c4cfd07460a33785b04
Ardelan <ardelanyamanel@outlook.de>
2024-07-15 20:37:56 +0200

    bump up version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 lua54 'yes'
 
 author 'Open Source by Monolith Vision and Contributors'
-version '1.1.0'
+version '1.1.1'
 description 'A modern interaction menu with Lua API made for FiveM.'
 
 ui_page 'web/dist/index.html'
